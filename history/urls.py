0aacf177ea2bc6a7d6bd809908a4f0041bb6103a
chabdullah <chabdullah@hotmail.it>
2019-08-24 19:43:11 +0200

    Aggiunto il grafico

--- history/urls.py
+++ history/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     # ex: /history/5/
     path('<int:material_id>/', views.detail, name='detail'),
-    path('<int:material_id>/<int:mesured_weight>/update', views.update, name='update')
+    path('<int:material_id>/<int:mesured_weight>/update', views.update, name='update'),
+    path('api/chart/data', views.ChartData.as_view())
+
 ]
--- history/views.py
+++ history/views.py
@@ -1,11 +1,20 @@
+from django.http import JsonResponse
+import json
+import objectpath
 from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404
 from django.views import generic
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from django.core import serializers
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import numpy as np
 
 # Create your views here.
 
-from .models import Materials
+from .models import Materials, ThrownTrash
 from django.utils import timezone
 
 class IndexView(generic.ListView):
@@ -30,4 +39,40 @@ def update(request, material_id, mesured_weight):
     new_trash = material.throwntrash_set.create(weight=mesured_weight, thrown_date=timezone.now())
     
     return HttpResponse("Database updated")
+
+
+
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, material_id=1, format=None):
+        material = ThrownTrash.objects.all()
+        material = serializers.serialize('json',material)
+
+        #for trash in trash_list:
+        #    default_items = np.append(default_items,trash.weight)
+
+
+        labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
+        default_items = [4,6,8,5,8,10,5,8,10,10,5,8,6,14,6]
+        data = {
+            "labels": labels,
+            "default": default_items,
+        }
+        return HttpResponse(material, content_type="application/json")
+        #return Response(data)
+
+
+
+
+
+
+
+
+
+
+
+
     
